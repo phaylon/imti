6402bdd64e542028798615e671d026230f939da1
Robert Sedlacek <rs@474.at>
2023-07-13 00:06:41 +0200

    switch edge via tab

--- demo/paned.rkt
+++ demo/paned.rkt
@@ -4,18 +4,26 @@
   "../main.rkt"
   ansi
   racket/set
+  racket/sequence
   racket/match)
+
+(define edges (in-cycle '(top right bottom left)))
+(define (current-edge) (sequence-ref edges 0))
+(define (next-edge!) (set! edges (sequence-tail edges 1)))
 
 (terminal-loop
   (lambda (f a)
-    (render-paned (render-clear f a) a 'top
+    (render-paned (render-clear f a) a (current-edge)
       (lambda (f a)
-        (render-text f a "Top Pane Area"))
+        (render-text f a (symbol->string (current-edge))))
       (lambda (f a)
-        (render-text f a "Body Area"))
+        (render-text f a "body"))
       #:min 5 #:max 15))
   (match-lambda
     ((== (key #\D (set 'control)))
      'break)
+    ((== (key #\I (set 'control)))
+     (next-edge!)
+     'redraw)
     (_ 'redraw)))
 
